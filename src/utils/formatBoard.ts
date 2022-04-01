74288f9eb38351ebe94168e90b1404becdbfc370
tkshio <tkshio>
2022-04-01 06:13:22 +0000

    BoardStateの出力関数を実装

--- src/utils/formatBoard.ts
+++ src/utils/formatBoard.ts
@@ -1,73 +1,93 @@
 import { BoardState } from '../BoardState'
 
-export function formatBoard(
-    board: BoardState,
-    red = 'red',
-    white = 'white'
-): string[] {
-    const points = 0
-    const cube = 1
-    const isRed = true
-    const upperIndexRow = '+12-11-10--9--8--7-------6--5--4--3--2--1-+'
-    const lowerIndexRow = '+13-14-15-16-17-18------19-20-21-22-23-24-+'
-    const isRedOnRoll = isRed ? 'On roll' : ''
-    const isWhiteOnRoll = isRed ? '' : 'On roll'
-    const cubeRow = '^|                  |BAR|                  |'
+/**
+ * format boardState to text in GNU Backgammon like style.
+ *
+ * @param board boardState to format
+ * @returns lines of formatted text
+ */
+export function formatBoard(board: BoardState): string[] {
+    const upperIndexRow = ' +12-11-10--9--8--7-------6--5--4--3--2--1-+   '
+    const lowerIndexRow = ' +13-14-15-16-17-18------19-20-21-22-23-24-+   '
+    const cubeRow = '^|                  |BAR|                  |   '
     return [
-        ` ${upperIndexRow}     O: ${red}`,
-        ` ${upperRow(0)}     ${points} points`,
-        ` ${upperRow(1)}     ${isRedOnRoll}`,
-        ` ${upperRow(2)}     `,
-        ` ${upperRow(3)}     `,
-        ` ${upperRow(4)}     `,
-        `${cubeRow}     (Cube: ${cube})`,
-        ` ${lowerRow(4)}     `,
-        ` ${lowerRow(3)}     `,
-        ` ${lowerRow(2)}     `,
-        ` ${lowerRow(1)}     ${isWhiteOnRoll}`,
-        ` ${lowerRow(0)}     ${points} points`,
-        ` ${lowerIndexRow}     X: ${white}`,
+        upperIndexRow,
+        upperRow(0),
+        upperRow(1),
+        upperRow(2),
+        upperRow(3),
+        upperRow(4, true),
+        cubeRow,
+        lowerRow(4, true),
+        lowerRow(3),
+        lowerRow(2),
+        lowerRow(1),
+        lowerRow(0),
+        lowerIndexRow,
     ]
 
-    function upperRow(n: number) {
-        const index = [...Array(12)].map((_, i, arr) => arr.length - i)
-        console.log('Upper', index)
-        return row(index, n)
+    function upperRow(n: number, mayOverflow = false) {
+        const pieces = [...Array(12)]
+            .map((_, i, arr) => arr.length - i)
+            .map((pos) => board.piecesAt(pos))
+        const onTheBar = board.piecesAt(25)
+        const bornOff = -board.opponentBornOff()
+        return row(pieces, n, onTheBar, bornOff, mayOverflow)
     }
-    function lowerRow(n: number) {
-        const index = [...Array(12)].map((_, i) => i + 13)
-        console.log('Lower', index)
-        return row(index, n)
+    function lowerRow(n: number, mayOverflow = false) {
+        const pieces = [...Array(12)]
+            .map((_, i) => i + 13)
+            .map((pos) => board.piecesAt(pos))
+        const onTheBar = board.piecesAt(0)
+        const bornOff = board.myBornOff()
+        return row(pieces, n, onTheBar, bornOff, mayOverflow)
     }
 
-    function row(index: number[], n: number) {
-        const redPiece = ' X '
-        const whitePiece = ' O '
-        const blank = '   '
+    function row(
+        pieces: number[],
+        rowNum: number,
+        onTheBar: number,
+        bornOff: number,
+        mayOverflow: boolean
+    ) {
+        const myPiece = 'X'
+        const oppPiece = 'O'
+        const blank = ' '
 
-        const left = index.slice(0, 6)
-        const right = index.slice(6, 12)
-        const print0 = (pos: number) => {
-            const c = board.piecesAt(pos)
-            return c > 10 || c < -10
-                ? `${c} `
-                : c > 5 || c < -5
-                ? ` ${c} `
-                : c > 0
-                ? redPiece
-                : c < 0
-                ? whitePiece
+        const printPiece = (pieces: number) =>
+            pieces > rowNum ? myPiece : pieces < -rowNum ? oppPiece : blank
+
+        const printOverflow = (pieces: number) =>
+            pieces >= 6 || pieces <= -6
+                ? `${Math.abs(pieces).toString(16).toUpperCase()}`
+                : pieces > rowNum
+                ? myPiece
+                : pieces < -rowNum
+                ? oppPiece
                 : blank
-        }
-        const print =
-            n === 0
-                ? print0
-                : (pos: number) =>
-                      board.piecesAt(pos) > n
-                          ? redPiece
-                          : board.piecesAt(pos) < -n
-                          ? whitePiece
-                          : blank
-        return `|${left.map(print).join('')}|   |${right.map(print).join('')}|`
+
+        const print = mayOverflow ? printOverflow : printPiece
+
+        const left = pieces.slice(0, 6).map(print).join('  ')
+        const right = pieces.slice(6, 12).map(print).join('  ')
+
+        const piecesOnTheBoard = ` | ${left} | ${print(onTheBar)} | ${right} |`
+
+        const bornOff0 = printPiece(bornOff)
+        const bornOff5 =
+            bornOff > 5
+                ? printPiece(bornOff - 5)
+                : bornOff < -5
+                ? printPiece(bornOff + 5)
+                : ' '
+        const bornOff10 =
+            bornOff > 10
+                ? printPiece(bornOff - 10)
+                : bornOff < -10
+                ? printPiece(bornOff + 10)
+                : ' '
+        const piecesBornOff = `${bornOff0}${bornOff5}${bornOff10}`
+
+        return piecesOnTheBoard + piecesBornOff
     }
 }
