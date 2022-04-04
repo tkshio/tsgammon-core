177a6c9a18634c014181d6b42a540bcd8c837892
tkshio <tkshio>
2022-04-04 05:22:31 +0000

    テストレポートの設定を追加

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['<rootDir>/dist/'],
+    reporters:[
+        'default',
+        ['jest-junit',{
+            suiteName: 'jest tests',
+            outputDirectory:'reports/jest',
+            outputName:'jest-test-results.xml',
+        }]
+    ]
 }
