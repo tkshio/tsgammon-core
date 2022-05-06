d0f4aeaab4595e1d0a6ead3c111cfc8cab0846dc
tkshio <tkshio>
2022-05-06 03:27:14 +0000

    ユニットテストの移管

---
+++ src/__tests__/dispatchers/CubeGameState_EOG.test.ts
@@ -0,0 +1,90 @@
+import { cube, CubeOwner } from '../../CubeState'
+import { resultToCBEoG } from '../../dispatchers/CubeGameState'
+import { eog } from '../../EOGStatus'
+import { SGResult } from '../../records/SGResult'
+
+describe('CBEoG', () => {
+    test('holds stake for winner', () => {
+        const cbEoG = resultToCBEoG(cube(1), SGResult.REDWON, eog())
+        expect(cbEoG.calcStake().stake.redScore).toBe(1)
+    })
+    test('holds stake multiplied by cube and gammon', () => {
+        const cbEoG = resultToCBEoG(
+            cube(4, CubeOwner.RED),
+            SGResult.WHITEWON,
+            eog({ isGammon: true })
+        )
+        expect(cbEoG.calcStake().stake.whiteScore).toBe(8)
+    })
+    test('holds stake multiplied by cube and Backgammon', () => {
+        const cbEoG = resultToCBEoG(
+            cube(4, CubeOwner.RED),
+            SGResult.WHITEWON,
+            eog({ isGammon: true, isBackgammon: true })
+        )
+        expect(cbEoG.calcStake().stake.whiteScore).toBe(12)
+    })
+})
+describe('jacoby rule', () => {
+    const conf = { jacobyRule: true }
+    test('disables gammon if cube is not used', () => {
+        const cbEoG = resultToCBEoG(
+            cube(1),
+            SGResult.WHITEWON,
+            eog({ isGammon: true })
+        )
+        const stakeCalc = cbEoG.calcStake(conf)
+        expect(stakeCalc.stake.whiteScore).toBe(1)
+        expect(stakeCalc.jacobyApplied).toBeTruthy()
+    })
+    test('disables backgammon if cube is not used', () => {
+        const cbEoG = resultToCBEoG(
+            cube(1),
+            SGResult.REDWON,
+            eog({ isGammon: true, isBackgammon: true })
+        )
+        const stakeCalc = cbEoG.calcStake(conf)
+        expect(stakeCalc.stake.redScore).toBe(1)
+        expect(stakeCalc.jacobyApplied).toBeTruthy()
+    })
+    test('enables gammon if cube is used', () => {
+        const cbEoG = resultToCBEoG(
+            cube(2, CubeOwner.WHITE),
+            SGResult.WHITEWON,
+            eog({ isGammon: true })
+        )
+        const stakeCalc = cbEoG.calcStake(conf)
+        expect(stakeCalc.stake.whiteScore).toBe(4)
+        expect(stakeCalc.jacobyApplied).toBeFalsy()
+    })
+    test('enables backgammon if cube is used', () => {
+        const cbEoG = resultToCBEoG(
+            cube(2, CubeOwner.WHITE),
+            SGResult.REDWON,
+            eog({ isGammon: true, isBackgammon: true })
+        )
+        const stakeCalc = cbEoG.calcStake(conf)
+        expect(stakeCalc.stake.redScore).toBe(6)
+        expect(stakeCalc.jacobyApplied).toBeFalsy()
+    })
+    test('recognizes cube usage by cube owner', () => {
+        const cbEoG = resultToCBEoG(
+            cube(2),
+            SGResult.WHITEWON,
+            eog({ isGammon: true })
+        )
+        const stakeCalc = cbEoG.calcStake(conf)
+        expect(stakeCalc.stake.whiteScore).toBe(2)
+        expect(stakeCalc.jacobyApplied).toBeTruthy()
+    })
+    test('recognizes cube usage by cube owner, not value', () => {
+        const cbEoG = resultToCBEoG(
+            cube(1, CubeOwner.RED),
+            SGResult.WHITEWON,
+            eog({ isGammon: true })
+        )
+        const stakeCalc = cbEoG.calcStake(conf)
+        expect(stakeCalc.stake.whiteScore).toBe(2)
+        expect(stakeCalc.jacobyApplied).toBeFalsy()
+    })
+})
