eeb15e875fd718040b63d0a1e7a0c4015af4e0c7
tkshio <tkshio>
2022-07-08 08:43:28 +0000

    SingleGameのEOGEventHandlerを追加

--- src/dispatchers/EOGEventHandlers.ts
+++ src/dispatchers/EOGEventHandlers.ts
@@ -1,12 +1,16 @@
 import { EOGStatus } from '../EOGStatus'
 import { SGResult } from '../records/SGResult'
-import { CubeGameListeners, cubeGameDispatcher } from './CubeGameDispatcher'
-import { CBState, CBEoG, CBResponse } from './CubeGameState'
-import { SingleGameListeners } from './SingleGameDispatcher'
+import { cubeGameDispatcher, CubeGameListeners } from './CubeGameDispatcher'
+import { CBState } from './CubeGameState'
+import {
+    singleGameDispatcher,
+    SingleGameListeners,
+} from './SingleGameDispatcher'
+import { SGState } from './SingleGameState'
 import { concat2 } from './utils/concat'
 
 export function eogEventHandlers(
-    listeners: Partial<SingleGameListeners & CubeGameListeners>[]
+    listeners: Partial<CubeGameListeners & SingleGameListeners>[]
 ) {
     const listener = {
         ...listeners.reduce((prev, cur) => concatEOGListeners(prev, cur)),
@@ -25,19 +29,42 @@ export function eogEventHandlers(
             result(listener)
         },
     }
-}
-function concatEOGListeners(
-    listener1: {
-        onEndOfCubeGame?: (state: CBEoG, lastState?: CBResponse) => void
-    },
-    listener2: {
-        onEndOfCubeGame?: (state: CBEoG, lastState?: CBResponse) => void
+    function concatEOGListeners(
+        listener1: Partial<Pick<CubeGameListeners, 'onEndOfCubeGame'>>,
+        listener2: Partial<Pick<CubeGameListeners, 'onEndOfCubeGame'>>
+    ) {
+        return {
+            onEndOfCubeGame: concat2(
+                listener1.onEndOfCubeGame,
+                listener2.onEndOfCubeGame
+            ),
+        }
     }
-) {
+}
+export function eogEventHandlersSG(listeners: Partial<SingleGameListeners>[]) {
+    const listener = {
+        ...listeners.reduce((prev, cur) => concatEOGListeners(prev, cur)),
+    }
     return {
-        onEndOfCubeGame: concat2(
-            listener1.onEndOfCubeGame,
-            listener2.onEndOfCubeGame
-        ),
+        onEndOfCubeGame: (
+            sgState: SGState,
+            sgResult: SGResult,
+            eog: EOGStatus
+        ) => {
+            const result = singleGameDispatcher.doEndOfGame(
+                sgState,
+                sgResult,
+                eog
+            )
+            result(listener)
+        },
+    }
+    function concatEOGListeners(
+        listener1: Partial<Pick<SingleGameListeners, 'onEndOfGame'>>,
+        listener2: Partial<Pick<SingleGameListeners, 'onEndOfGame'>>
+    ) {
+        return {
+            onEndOfGame: concat2(listener1.onEndOfGame, listener2.onEndOfGame),
+        }
     }
 }
